f5dd531f8582920e4410f773d4e560c1a42bd9d5
Riaz Ahamed <ariaz56@jnn.edu.in>
2026-09-24 17:41:16 +0530

    Add user model

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true, maxlength: 80 },
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true, minlength: 6, select: false },
+  role: { type: String, enum: ["student", "admin"], default: "student" }
+}, { timestamps: true });
+
+userSchema.pre("save", async function save(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.comparePassword = function comparePassword(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
